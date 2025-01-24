a6154ac56ec49f575b2c32884b7dd794937e627e
AlexSvet117 <alex.svet117@gmail.com>
2025-01-24 16:07:45 +0000

    WIP

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -1,4 +1,18 @@
+import React, { useState } from 'react';
+
 function TrafficLight() {
+
+  // State to track which circle has the white shadow class
+  const [activeCircle, setActiveCircle] = useState(null)
+
+  // // Function to toggle the shadow on click
+  // const handleCircleClick = (color) => {
+  //   if (activeCircle === color) {
+  //     setActiveCircle(null); // Deselect if the same circle is clicked again
+  //   } else {
+  //     setActiveCircle(color); // Set the active circle
+  //   }
+  // };
 
   return (
   <div className="d-flex flex-column flex-md-row p-1 mt-lg-5 gap-4 py-md-1 justify-content-center align-items-center bg-black rounded-4">
