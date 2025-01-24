4da80467935279fdcc8cc353af5f55b3fbad96ba
AlexSvet117 <alex.svet117@gmail.com>
2025-01-24 15:49:01 +0000

    Main TrafficLight html structure and custom css class

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -1,8 +1,14 @@
 function TrafficLight() {
+
   return (
-    <div className="containter bg-black border-2 row justify-content-center">
-      
+  <div className="d-flex flex-column flex-md-row p-1 mt-lg-5 gap-4 py-md-1 justify-content-center align-items-center bg-black rounded-4">
+    <div className="list-group">
+        <div className="rounded-circle bg-danger m-1" style={{ width: '100px', height: '100px' }}></div>
+        <div className="rounded-circle bg-warning m-1" style={{ width: '100px', height: '100px' }}></div>
+        <div className="rounded-circle bg-success m-1" style={{ width: '100px', height: '100px' }}></div>
     </div>
+  </div>
+
   )
 }
 
